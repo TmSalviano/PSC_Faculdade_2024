e1deeca0a05868a143e282fe3dca993775824ba2
TmSalviano <tiagomsalviano@outlook.com>
2024-09-14 11:46:37 -0300

    Adding Ex3 and Ex4 of Pratica02 Wesley's List

--- AulaDeQuinta/Pratica02_EstruturaCondicional/Tiago/Ex3/Main.java
+++ AulaDeQuinta/Pratica02_EstruturaCondicional/Tiago/Ex3/Main.java
@@ -3,9 +3,35 @@ import java.util.Scanner;
 public class Main {
     public static void main (String args[]) {
         try (var keyboard = new Scanner(System.in)) {
-            
+            System.out.println("Insira a primeira nota:");
+            var nota1 = inputLimiter(keyboard);
+
+            System.out.println("Insira a segunda nota:");
+            var nota2 = inputLimiter(keyboard);
+
+            System.out.println("Insira a terceira nota:");
+            var nota3 = inputLimiter(keyboard);
+
+            var media = (nota1 + nota2 + nota3) / 3;
+            if (media >= 0 && media < 3)
+                System.err.println("Reprovado");
+            else if (media >= 3 && media < 7)
+                System.err.println("Exame");
+            else
+                System.err.println("Aprovado");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+
+    public static int inputLimiter(Scanner keyboard) {
+        var num = keyboard.nextInt();
+        while (num < 0 || num > 10)
+        {
+            System.out.println("Invalid number. Type a number from 0 to 10:");
+            num = keyboard.nextInt();
+        }
+        return num;
     }
 }
--- AulaDeQuinta/Pratica02_EstruturaCondicional/Tiago/Ex4/Main.java
+++ AulaDeQuinta/Pratica02_EstruturaCondicional/Tiago/Ex4/Main.java
@@ -3,7 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main (String args[]) {
         try (var keyboard = new Scanner(System.in)) {
-            
+            System.out.println("Insira o seu peso:");
+            var peso = keyboard.nextFloat();
+            System.out.println("Insira a sua altura:");
+            var altura = keyboard.nextFloat();
+
+            var imc = (int)(peso / Math.pow(altura, 2));
+            if (imc < 20) {
+                System.out.println("Abaixo do Peso");
+            } else if (imc >= 20 && imc < 25) {
+                System.out.println("Normal");
+            } else if (imc >= 25 && imc < 30) {
+                System.out.println("Sobrepeso");
+            } else if (imc >= 30 && imc <= 40) {
+                System.out.println("Obesidade");
+            } else if (imc > 40) {
+                System.out.println("Obesidade Morbida");
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
